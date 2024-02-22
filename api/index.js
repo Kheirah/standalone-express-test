11169a21c65b289c28b0dff39c9d18d6f5d0a921
Ernst Stolz <eric@spiritsdontfly.com>
2024-02-22 12:12:48 +0100

    add middleware

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const app = express();
 app.use("/:user", user);
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/:id", async (req, res) => {
   await createTables();
